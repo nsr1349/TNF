4d90719805dab4f479521bbf46a6ef3ec6572fce
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-10 23:58:47 +0900

    feat: 디테일 페이지 마크업 중

--- src/app/boardgame/[id]/page.tsx
+++ src/app/boardgame/[id]/page.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image"
+import Link from "next/link"
+import { BoardgameDetailItem } from "./mocks"
+
 interface BoardgameDetailProps {
   params : { 
     id : string
@@ -6,10 +10,55 @@ interface BoardgameDetailProps {
 
 export default function BoardgameDetail({ params }: BoardgameDetailProps) {
   const { id } = params
+  const {_id, title, ageCut, brand, price, deliveryCharge, explain, genre, image, peopleCut, playTime, point, relatedProducts, review } = BoardgameDetailItem
+  console.log(id)
+
   return (
     <>
-        <h1>{id}</h1>
-        디테일 페이지
+      <section>
+        <div>
+          <div className='aspect-square overflow-hidden'>
+              <Image className="h-full object-cover" src={image} alt={title} width={500} height={500}/>
+          </div>
+          <div className='overflow-hidden'>
+              {/* <Image className="h-full object-cover" src={'https://popcontr2632.cdn-nhncommerce.com/data/goods/23/11/48/93401/93401_main_03.jpg'} alt="너구리" width={500} height={500}/> */}
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <h1>{title}</h1>
+              <span>{`${ageCut}세 이상, ${peopleCut}, ${playTime}`}</span>
+            </div>
+            <div>d</div>
+          </div>
+          <table>
+              <tr>
+                <td>브랜드</td>
+                <td>{brand}</td>
+              </tr>
+              <tr>
+                <td>장르</td>
+                <td>{genre}</td>
+              </tr>
+              <tr>
+                <td>베송비</td>
+                <td>{deliveryCharge}</td>
+              </tr>
+              <tr>
+                <td>적립금</td>
+                <td>{point}</td>
+              </tr>
+          </table>
+          <hr />
+          <h1>{price}</h1>
+          <button></button>
+        </div>
+      </section>
+      <section>
+
+      </section>
     </>
   );
 }
+
