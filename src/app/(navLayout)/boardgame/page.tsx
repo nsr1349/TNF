c0e846c91e225713a4c3d702d0acc63d9dbd5c2e
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-19 12:20:19 +0900

    faet: 보드게임 페이지 url 쿼리대로 필터구현

--- src/app/(navLayout)/boardgame/page.tsx
+++ src/app/(navLayout)/boardgame/page.tsx
@@ -1,18 +1,33 @@
-import Card from "../component/Card";
+import Card from "../../component/Card";
 import { FaFilter, FaSearch  } from "react-icons/fa";
 import Link from "next/link";
+import { createClient } from "@/utils/supabase/client";
 
 interface BoardgameProps {
   searchParams : { 
-    page? : number,
-    genre? : string,
-    people? : string,
+    page? : number
+    genre? : string
+    people? : string
     brand? : string
+    search? : string
   }
 }
 
-export default function Boardgame({ searchParams } : BoardgameProps) {
-  const { page = 1, genre, people, brand } = searchParams
+export default async function Boardgame({ searchParams } : BoardgameProps) {
+  const { page = 1, genre, people, brand, search = '' } = searchParams
+  const supabase = createClient();
+  let query = supabase
+  .from('boardGame')
+  .select('id,title,image,price,brand', { count: 'exact' })
+
+  if (search) query = query.or(`title.ilike.%${search}%,brand.ilike.%${search}%`)
+  if (genre) query = query.eq('genre', genre)
+  if (people) query = query.eq('people', people)
+  if (brand) query = query.eq('brand', brand)
+  query = query.range((page - 1) * 16 , (page * 16) - 1)
+
+  const { data: boardGame, error, count } = await query
+  console.log(boardGame)
 
   return (
     <>
@@ -26,21 +41,21 @@ export default function Boardgame({ searchParams } : BoardgameProps) {
           {people ? <span className="text-indigo-600">{people}</span> : ''}
           {brand ? <span className="text-indigo-600">{brand}의</span> : ''}
           {genre ? <span className="text-indigo-600">{genre}</span> : ''}
-          게임만 보기!
+          게임만 보기! 총 {count}개 
           </> : <span className="text-zinc-500">필터 미적용</span>
         }
       </h3>
       <div className="relative">
-        <input type="text" className="bg-[rgba(0,0,0,0)] border-zinc-800 border-1 p-2 px-4 pr-12 rounded-full focus:outline-none focus:bg-zinc-900"/>
+        <input value={search} type="text" className="bg-[rgba(0,0,0,0)] border-zinc-800 border-1 p-2 px-4 pr-12 rounded-full focus:outline-none focus:bg-zinc-900"/>
         <div className="absolute right-4 inset-y-0 content-center text-xl">
           <FaSearch/>
         </div>
       </div>
     </section>
     <section className="grid grid-cols-4 gap-x-8 gap-y-20">
-        {Array.from({ length: 12 }, (_, index) => (
-              <Card key={index} href="/boardgame/1"/>
-        ))}
+        { boardGame?.map((boardGameData)=> 
+          <Card key={boardGameData.id} {...boardGameData}/>)
+        }
     </section>
     <section className="mt-10">
       <Link href={{query: { ...searchParams, page: 2}}}>2</Link>
