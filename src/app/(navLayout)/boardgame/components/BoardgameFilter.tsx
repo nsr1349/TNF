194f9591f5f29f5f7c5a6481110fb9e90474d80e
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-25 23:29:07 +0900

    feat: boardgame 필터를 폼 컴포넌트로로 분리

--- src/app/(navLayout)/boardgame/components/BoardgameFilter.tsx
+++ src/app/(navLayout)/boardgame/components/BoardgameFilter.tsx
@@ -45,78 +45,73 @@ export default function BoardgameFilter({ searchParams, count } : BoardgameFilte
     }
 
     const filterAction = (formData : FormData) => {
-        console.log(formData)
-    }
-
-
-    const handleSubmit = (formData : FormData) => {
         const genre = formData.get('장르')
         const people = formData.get('인원')
         const brand = formData.get('브랜드')
-        console.log(genre, people, brand)
-        router.push(`?${!!genre ? `genre=${genre}&` : ''}${!!people ? `people=${people}&` : ''}${!!brand ? `brand=${brand}&` : ''} `);
+        const search = formData.get('search')
+
+        router.push(`?${!!genre ? `genre=${genre}&` : ''}${!!people ? `people=${people}&` : ''}${!!brand ? `brand=${brand}&` : ''}${!!search ? `search=${search}` : ''}`)
+        setIsOpenModal(false)
     }
 
     return (          
-        <section className="flex w-full items-center max-w-[900px] my-10">
-            {isOpenModal && 
-                <div className="fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.5)] z-50 center">
-                    <div className="bg-main w-4/5 max-w-[1000px] px-10 py-10 relative">
-                        <div className="absolute top-10 right-10 text-4xl" onClick={()=>setIsOpenModal(false)}>
-                            <IoCloseOutline />
-                        </div>
-                        <div className="flex items-center">
-                            <FaFilter className='text-purewhite'/>
-                            <h3 className="flex-grow ml-4 font-bold text-lg">필터</h3>
-                        </div>
-                        <form action={handleSubmit}>
-                            <div className="flex gap-10 my-12 text-xl" onClick={handleRadioChecked}>
-                            {filterOptions.map(({Name, options})=>
-                                <div key={Name}>
-                                    <h3 className="font-bold">{Name}</h3> 
-                                    <ul className="space-y-4 pt-4">
-                                        {options.map((optionName)=>
-                                            <li key={optionName} > 
-                                                <input type="radio" name={Name} defaultValue={optionName} id={optionName} className="hidden peer" />
-                                                <label htmlFor={optionName} className="peer-checked:text-point peer-checked:font-bold text-gray select-none hover:text-zinc-400" >{optionName}</label>
-                                            </li>
-                                        )}
-                                    </ul>
+        <section className="w-full max-w-[900px] py-10 px-4">
+            <form action={filterAction} className="flex w-full items-center max-w-[900px]">
+                {isOpenModal && 
+                    <div className="fixed top-0 left-0 w-screen h-screen bg-[rgba(0,0,0,0.5)] z-50 center">
+                        <div className="bg-main w-4/5 max-w-[1000px] px-10 py-10 relative">
+                            <div className="absolute top-10 right-10 text-4xl" onClick={()=>setIsOpenModal(false)}>
+                                <IoCloseOutline />
+                            </div>
+                            <div className="flex items-center">
+                                <FaFilter className='text-purewhite'/>
+                                <h3 className="flex-grow ml-4 font-bold text-lg">필터</h3>
+                            </div>
+                            <div >
+                                <div className="flex gap-10 my-12 text-xl" onClick={handleRadioChecked}>
+                                {filterOptions.map(({Name, options})=>
+                                    <div key={Name}>
+                                        <h3 className="font-bold">{Name}</h3> 
+                                        <ul className="space-y-4 pt-4">
+                                            {options.map((optionName)=>
+                                                <li key={optionName} > 
+                                                    <input type="radio" name={Name} defaultValue={optionName} id={optionName} className="hidden peer" />
+                                                    <label htmlFor={optionName} className="peer-checked:text-point peer-checked:font-bold text-gray select-none hover:text-zinc-400" >{optionName}</label>
+                                                </li>
+                                            )}
+                                        </ul>
+                                    </div>
+                                )}
                                 </div>
-                            )}
+                                <div className="text-right">
+                                    <button type="submit" className="bg-purewhite text-main text-lg py-2 px-10 font-bold hover:bg-zinc-400 transition-all">적용하기</button>
+                                </div>
                             </div>
-                            <div className="text-right">
-                                <button type="submit" className="bg-purewhite text-main text-lg py-2 px-10 font-bold hover:bg-zinc-400 transition-all">적용하기</button>
-                            </div>
-                        </form>
+                        </div>
                     </div>
-                </div>
-            }
-            <div onClick={() => setIsOpenModal(true)} className="flex items-center flex-grow">
-                <div>
-                    <FaFilter className={`${ genre || people || brand ? 'text-purewhite' : 'text-gray'}`}/>
-                </div>
-                <h3 className="flex-grow ml-4 font-bold text-lg">
-                {genre || people || brand ? <>
-                    {people && <span className="text-point">{people} </span>}
-                    {brand && <span className="text-point">{brand}의 </span>}
-                    {genre && <span className="text-point">{genre} </span>}
-                    게임만 보기! 
-                    {count ? `총 ${count}개` : ''}
-                    </> : <span className="text-gray">필터 미적용</span>
                 }
-                </h3>
-            </div>
-
-            <div className="relative">
-                <input 
-                    type="text" 
-                    className="bg-[rgba(0,0,0,0)] border-sub border-1 p-2 px-4 pr-12 rounded-full focus:outline-none focus:main"
-                />
-                <div className="absolute right-4 inset-y-0 content-center text-xl">
-                    <FaSearch/>
+                <div onClick={() => setIsOpenModal(true)} className="flex items-center flex-grow">
+                    <div>
+                        <FaFilter className={`${ genre || people || brand ? 'text-purewhite' : 'text-gray'}`}/>
+                    </div>
+                    <h3 className="flex-grow ml-4 font-bold text-lg">
+                    {genre || people || brand ? <>
+                        <span className="text-point">{people && people} {brand && brand + '의 '} {genre && genre}</span> 
+                        <span> 게임만 보기! {count ? ` 총 ${count}개` : ''}</span>
+                        </> : <span className="text-gray">필터 미적용</span>}
+                    </h3>
                 </div>
-            </div>
+                <div className="relative">
+                    <input 
+                        type="text" 
+                        name='search'
+                        className="bg-[rgba(0,0,0,0)] border-sub border-1 p-2 px-4 pr-12 rounded-full focus:outline-none focus:main"
+                    />
+                    <button className="absolute right-4 inset-y-0 content-center text-xl" type="submit" value={search}>
+                        <FaSearch/>
+                    </button>
+                </div>
+            </form>
         </section>
     );
 }
