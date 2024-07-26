f2df775c421446a09b1c8a3ab03658438ff365f3
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-26 23:49:35 +0900

    feat: navbar 반응형 적용

--- src/app/component/Navbar.tsx
+++ src/app/component/Navbar.tsx
@@ -2,19 +2,22 @@
 
 import { usePathname } from 'next/navigation'
 import Link from "next/link";
+import { MdHomeFilled } from "react-icons/md";
+import { IoIosPaper, IoIosHelpCircle  } from "react-icons/io";
 
 export default function Navbar() {
     const path = usePathname()
 
     return (
-        <nav className="flex gap-4 h-full flex-grow">
+        <nav className="sm:border-t-1 sm:border-sub sm:fixed sm:bottom-0 sm:left-0 bg-main sm:w-full sm:h-16 relative w-fit flex gap-4 h-full flex-grow">
             {
-                navList.map(({href,text})=> 
+                navList.map(({href,text,icon})=> 
                     <Link 
                         key={href} 
                         href={href} 
-                        className={"h-full px-4 font-bold transition-all grid place-items-center hover:text-purewhite " + (path.indexOf(href) === 0 ? 'text-purewhite' : 'text-gray')} >
-                        {text}
+                        className={"center px-4 font-bold transition sm:flex-grow hover:text-purewhite " + (path.indexOf(href) === 0 ? 'text-purewhite' : 'text-gray')} >
+                        <span className='text-2xl hidden sm:block'>{ icon }</span>
+                        <span>{text}</span>
                     </Link>
                 )
             }
@@ -25,14 +28,17 @@ export default function Navbar() {
 const navList = [
     {
         href : '/boardgame',
-        text : '보드게임'
+        text : '보드게임',
+        icon : <MdHomeFilled/>
     },
     {
         href : '/news',
-        text : '새소식'
+        text : '새소식',
+        icon : <IoIosPaper/>
     },
     {
         href : '/about',
-        text : '소개'
+        text : '소개',
+        icon : <IoIosHelpCircle/>
     },
 ]
