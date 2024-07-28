2c62b2503db4e71f239d4bed6e13a19461a27b61
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-29 04:18:15 +0900

    feat: cart 퍼블리싱

--- src/app/(navLayout)/cart/components/CartItem.tsx
+++ src/app/(navLayout)/cart/components/CartItem.tsx
@@ -1,11 +1,12 @@
 'use client'
 import { ProductType } from "../page"
+import Image from "next/image"
+import { IoCloseOutline } from "react-icons/io5";
 
 interface CartItemProps {
     product : ProductType
     handleCartQuantity : (targetId : string , num : number)=> void
 }
-
 
 export default function CartItem({ product, handleCartQuantity, ...props } : CartItemProps) {
     const { id , image, price, quantity, title } = product
@@ -13,22 +14,21 @@ export default function CartItem({ product, handleCartQuantity, ...props } : Car
         <>
             <div className="flex" {...props}>
                 <div>
-                    이미지
+                    <Image src={image} alt={title} width={100} height={100}></Image>
                 </div>
-                <div className="flex-grow">
-                    <h3>{title}</h3>
-                    <p>{price}</p>
+                <div className="flex-grow content-center">
+                    <h3 className="font-bold">{title}</h3>
+                    <p className="text-gray mb-2">{price}</p>
                     <div>
                         <button className="bg-sub hover:bg-subhover px-2" onClick={()=> handleCartQuantity(id , -1)} type="button">-</button>
                         <input type="text" value={quantity} className="w-10 text-center bg-main" name={title} readOnly/>
                         <button className="bg-sub hover:bg-subhover px-2" onClick={()=> handleCartQuantity(id, + 1)} type="button">+</button>
                     </div>
                 </div>
-                <div>
-                    <div>닫기</div>
-                    <h2>{price*quantity}</h2>
+                <div className="relative content-end">
+                    <IoCloseOutline className="text-3xl absolute top-0 right-0 "/>
+                    <h2 className="text-xl">{price*quantity}</h2>
                 </div>
-                
             </div>
         </>
     );
--- src/app/(navLayout)/cart/page.tsx
+++ src/app/(navLayout)/cart/page.tsx
@@ -15,21 +15,21 @@ export interface ProductType {
 const mock = [
     {
         id : '1',
-        image : '/덤불속.png',
+        image : '/dumb_bul_sock.png',
         title : '덤불속',
         quantity : 1,
         price : 28800
     },
     {
         id : '2',
-        image : '/덤불속.png',
+        image : '/dumb_bul_sock.png',
         title : '덤불속',
         quantity : 1,
         price : 28800
     },
     {
         id : '3',
-        image : '/덤불속.png',
+        image : '/dumb_bul_sock.png',
         title : '덤불속',
         quantity : 1,
         price : 28800
@@ -38,6 +38,7 @@ const mock = [
 
 export default function Cart() {
     const [ cart , setCart ] = useState(mock)
+    const productPrice = cart.reduce((acc, {price,quantity})=> acc + (price * quantity) , 0)
 
     const handleCartQuantity = (targetId : string , num : number)=> {
         const newCart = cart.map((product)=> {
@@ -51,16 +52,29 @@ export default function Cart() {
     return (
             <div className="w-full max-w-[1000px] px-6">
                 <h1 className="my-6 text-xl font-bold">장바구니</h1>
-                <form action={orderAction}>
-                    {cart.map((product)=> <div key={product.id}>
-                        <CartItem product={product} handleCartQuantity={handleCartQuantity}/>
-                        <hr className="border-sub my-4"/>
-                    </div> )}
-                    <div>
-                        <h2>총수량</h2>
-                        <h2>{ cart.reduce((acc, {price,quantity})=> acc + (price * quantity) , 0) } 원</h2>
+                <form action={orderAction} className="flex gap-4">
+                    <div className="flex-grow">
+                        {cart.map((product)=> <div key={product.id}>
+                            <CartItem product={product} handleCartQuantity={handleCartQuantity}/>
+                            <hr className="border-sub my-4"/>
+                        </div> )}
                     </div>
-                    <button className="bg-purewhite text-main hover:bg-zinc-300 px-14 py-3 font-bold">주문하기</button>
+                    <div className="px-4 w-60 flex flex-col gap-1">
+                        <div className="flex justify-between">
+                            <h2 className="text-gray">상품가격</h2>
+                            <h2>{ productPrice }</h2>
+                        </div>
+                        <div className="flex justify-between">
+                            <h2 className="text-gray">배송비</h2>
+                            <h2>2500</h2>
+                        </div>
+                        <hr className="my-2"/>
+                        <div className="flex justify-between">
+                            <h2 className="text-gray">총 가격</h2>
+                            <h2>{ productPrice + 2500}</h2>
+                        </div>
+                        <button className="bg-purewhite text-main hover:bg-zinc-300 w-full py-2 mt-2 font-bold">주문하기</button>
+                    </div>
                 </form>
             </div>
         );
