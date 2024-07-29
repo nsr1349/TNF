54fb93c05f2d10210577409a7762d055e2344f7e
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-07-29 21:34:34 +0900

    feat: cart page 반응형 적용

--- src/app/(navLayout)/cart/page.tsx
+++ src/app/(navLayout)/cart/page.tsx
@@ -52,14 +52,14 @@ export default function Cart() {
     return (
             <div className="w-full max-w-[1000px] px-6">
                 <h1 className="my-6 text-xl font-bold">장바구니</h1>
-                <form action={orderAction} className="flex gap-4">
+                <form action={orderAction} className="flex gap-10 sm:flex-col sm:gap-4">
                     <div className="flex-grow">
                         {cart.map((product)=> <div key={product.id}>
                             <CartItem product={product} handleCartQuantity={handleCartQuantity}/>
                             <hr className="border-sub my-4"/>
                         </div> )}
                     </div>
-                    <div className="px-4 w-60 flex flex-col gap-1">
+                    <div className="w-60 flex flex-col gap-1 sm:w-full sm:text-xl">
                         <div className="flex justify-between">
                             <h2 className="text-gray">상품가격</h2>
                             <h2>{ productPrice }</h2>
@@ -68,12 +68,12 @@ export default function Cart() {
                             <h2 className="text-gray">배송비</h2>
                             <h2>2500</h2>
                         </div>
-                        <hr className="my-2"/>
+                        <hr className="my-4"/>
                         <div className="flex justify-between">
                             <h2 className="text-gray">총 가격</h2>
                             <h2>{ productPrice + 2500}</h2>
                         </div>
-                        <button className="bg-purewhite text-main hover:bg-zinc-300 w-full py-2 mt-2 font-bold">주문하기</button>
+                        <button className="bg-purewhite text-main hover:bg-zinc-300 w-full py-2 mt-4 font-bold">주문하기</button>
                     </div>
                 </form>
             </div>
