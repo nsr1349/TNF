c4240eeb2ac464c509174f2b196abba81ef681e8
nsr1349 <79898508+nsr1349@users.noreply.github.com>
2024-08-22 21:25:09 +0900

    feat: 리다이렉트 오류 수정 및 장바구니 추가 성공시 모달 오픈

--- src/app/(navLayout)/boardgame/[id]/components/AddCartBtn.tsx
+++ src/app/(navLayout)/boardgame/[id]/components/AddCartBtn.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { redirect } from 'next/navigation';
+import { useRouter } from 'next/navigation'
 import { supabase } from '@/utils/supabase/client';
+import Modal from '@/app/component/Modal';
+import { useModal } from '@/app/hooks/useModal';
 
 interface AddCartBtnProps {
     product_id : string
@@ -10,18 +12,32 @@ interface AddCartBtnProps {
 }
 
 export default function AddCartBtn({product_id , image , user_id} : AddCartBtnProps)  {
+    const { isOpen, openModal, closeModal } = useModal()
+    const router = useRouter()
+
     const AddCart = async () => {
-        if (!user_id) redirect('/login');
+        if (!user_id) return router.push("/login");
         
         const { data, error } = await supabase
         .from('cart')
         .insert([{ user_id, product_id , image }])
         .select()
-        console.log(data)
+        
+        if (error) return console.log('무언가 잘못되었습니다..')
+        openModal()
+        
     }
 
 
     return (
-        <button onClick={AddCart} className="w-full bg-purewhite text-main h-14 font-bold py-2 hover:bg-zinc-400 transition-all duration-200">장바구니 담기</button>
+        <>
+            <Modal isOpen={isOpen} closeModal={closeModal}>
+                <h1>장바구니에 담겼습니다!</h1>
+                <p>수량은 장바구니에서 변경 가능합니다</p>
+                <button onClick={()=> router.push('/cart')}>장바구니 바로가기</button>
+            </Modal>
+            <button onClick={AddCart} className="w-full bg-purewhite text-main h-14 font-bold py-2 hover:bg-zinc-400 transition-all duration-200">장바구니 담기</button>
+        </>
+
     );
 }
